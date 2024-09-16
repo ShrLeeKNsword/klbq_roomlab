87d69a3f448d3120662cdab3eb48ee59bedde9d2
爱走位的KN_sword <3478099037@qq.com>
2024-09-16 21:42:06 +0800

    666

--- src/component/mainpage.tsx
+++ src/component/mainpage.tsx
@@ -3,7 +3,7 @@ import CataBox from "@/component/catalogry_list";
 
 export default function MainPage() {
     return (
-        <div className="h-full w-screen pt-10 overflow-y-scroll overflow-x-hidden grid grid-cols-1 place-content-between" style={{minWidth:"700px",minHeight:"700px"}}>
+        <div className="h-full w-full pt-10 overflow-y-scroll overflow-x-hidden grid grid-cols-1 place-content-between" style={{minWidth:"700px",minHeight:"700px"}}>
             <div className="text-3xl font-bold pt-4 w-full h-max flex content-center justify-center">卡拉彼丘自建房开房助手</div>
             <RandomBox />
             <CataBox />
